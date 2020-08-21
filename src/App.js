bd0b410c6a291c761058a047437ecd9a723993b4
Vanger-Li <64912226+Vanger-Li@users.noreply.github.com>
2020-08-21 18:21:23 +0500

    create-component-card

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Click Memory Game</h1>
+      <h2>Can you remember where the cards are?</h2>
+    </div>
+  );
+}
