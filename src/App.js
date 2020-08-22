df94956455a3c2f41337169db4e63cc4213a9348
Vanger-Li <64912226+Vanger-Li@users.noreply.github.com>
2020-08-22 13:16:54 +0500

    refactoring

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ import "./styles.css";
 export default function App() {
   const [flipped, setFlipped] = useState([]);
 
-  const handleClick = (id) => setFlipped(id);
+  const handleClick = (id) => setFlipped([...flipped, id]);
 
   return (
     <div className="App">
       <h1>Click Memory Game</h1>
       <h2>Can you remember where the cards are?</h2>
       <Card
-        id = {1}
-        height = {100}
-        width = {100}
-        back = {"/images/react-backside.png"}
-        flipped = {flipped.includes(1)}
-        handleClick = {() => handleClick(1)}
+        id={1}
+        height={100}
+        width={100}
+        back={"/images/react-backside.png"}
+        flipped={flipped.includes(1)}
+        handleClick={() => handleClick(1)}
       />
     </div>
   );
